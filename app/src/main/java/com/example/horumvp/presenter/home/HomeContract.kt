4fd19def35e0ddfc2e2823d40dc01e4a620593d3
RodrigoBarbosaa <rodrigobarbosadeoliveira29@gmail.com>
2025-03-22 13:36:29 -0300

    Propriedades do usuário mostradas na tela home (NAO TESTADO)

--- app/src/main/java/com/example/horumvp/presenter/home/HomeContract.kt
+++ app/src/main/java/com/example/horumvp/presenter/home/HomeContract.kt
@@ -1,11 +1,16 @@
 package com.example.horumvp.presenter.home
 
+import com.example.horumvp.model.repository.Property
+
 interface HomeContract {
     interface View {
         fun onLogoutSuccess()
+        fun displayProperties(properties: List<Property>)
     }
 
     interface Presenter {
         fun logout()
+        fun loadProperties()
+        fun updatePaymentStatus(propertyId: String, newStatus: Boolean)
     }
 }
